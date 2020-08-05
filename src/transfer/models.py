5f01f037bff01ce8f330c0ddbe9320d7fd1321e7
Roman Zhidkov <arist0.rr@gmail.com>
2020-08-05 12:17:22 +0300

    Fix erratic Transfer.do_transfer() behavior

--- src/transfer/models.py
+++ src/transfer/models.py
@@ -1,9 +1,19 @@
 from decimal import Decimal
 from django.db import models
+from django.db.models import F
+
 from account.models import Account
 
 
-class InsufficientBalance(Exception):
+class InvalidAccounts(Exception):
+    pass
+
+
+class InsufficientFunds(Exception):
+    pass
+
+
+class InvalidAmount(Exception):
     pass
 
 
@@ -14,14 +24,26 @@ class Transfer(models.Model):
     amount = models.DecimalField(max_digits=18, decimal_places=2)
 
     @staticmethod
-    def do_transfer(from_account: Account, to_account: Account, amount: Decimal):
-        if from_account.balance < amount:
-            raise InsufficientBalance()
+    def validate_transaction_params(from_account: Account, to_account: Account, amount: Decimal):
+        if any([from_account is None,
+                to_account is None,
+                from_account == to_account]):
+            raise InvalidAccounts()
+        if amount <= 0:
+            raise InvalidAmount()
 
-        from_account.balance -= amount
-        to_account.balance += amount
+    @staticmethod
+    def do_transfer(from_account: Account, to_account: Account, amount: Decimal) -> object:
+        Transfer.validate_transaction_params(from_account, to_account, amount)
+        is_updated = Account.objects.filter(
+            id=from_account.id,
+            balance__gte=amount
+        ).update(balance=F('balance') - amount)
 
-        from_account.save()
+        if not is_updated:
+            raise InsufficientFunds()
+
+        to_account.balance = F('balance') + amount
         to_account.save()
 
         return Transfer.objects.create(
